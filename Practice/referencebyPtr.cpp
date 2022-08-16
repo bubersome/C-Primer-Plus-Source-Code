8c2e8311d53ca9c211eaf55a52466f28a68f4064
bubersome <bubersome@gmail.com>
2022-08-16 09:15:06 -0500

    runoob pointer reference

---
+++ Practice/referencebyPtr.cpp
@@ -0,0 +1,32 @@
+//
+// Created by thundermac on 8/16/22.
+//
+
+#include "referencebyPtr.h"
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+// 在写函数时应习惯性的先声明函数，然后在定义函数
+void getSeconds(unsigned long *par);
+
+int main ()
+{
+    unsigned long sec;
+
+
+    getSeconds( &sec );
+
+    // 输出实际值
+    cout << "Number of seconds :" << sec << endl;
+
+    return 0;
+}
+
+void getSeconds(unsigned long *par)
+{
+    // 获取当前的秒数
+    *par = time( NULL );
+    return;
+}
